f1d7ed0e3b7146d4f5511b6db3492112071755b3
Gabriel Manika <gmkoeb@gmail.com>
2024-09-30 21:28:29 -0300

    Add character listing to home page
    - Installed and configured axios for API calls
    - Created the first version of character listing in the front page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,44 @@
+import { useEffect, useState } from "react";
+import { api } from "../api/axios";
+
+
 function App() {
+  const [characters, setCharacters] = useState([]);
+  const [info, setInfo] = useState({})
+  const [currentPage, setCurrentPage] = useState('/character/?page=1');
+
+  async function getCharacters(){
+    const response = await api.get(currentPage);
+    setCharacters(response.data.results);
+    setInfo(response.data.info);
+  }
+  
+  useEffect(() => {
+    getCharacters()
+  }, [currentPage]);
+
+  function nextPage(){
+    if (info.next !== null) {
+      setCurrentPage(info.next);
+    }
+  }
+
+  function previousPage(){
+    if (info.prev !== null) {
+      setCurrentPage(info.prev);
+    }
+  }
+  
   return (
     <>
+      <h1>Rick & Morty Characters</h1>
+      {characters.map(character => (
+        <div key={character.id}>
+          <h1>{character.name}</h1>
+        </div>
+      ))}
+      <button onClick={nextPage}>next</button>
+      <button onClick={previousPage}>prev</button>
     </>
   )
 }
